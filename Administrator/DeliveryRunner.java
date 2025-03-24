a6eb10020cbe09edce37511e488374aeaa0ac4b7
Uma <27pupaley@gmail.com>
2025-03-24 15:02:06 +0500

    Create DeliveryRunner.java

---
+++ Administrator/DeliveryRunner.java
@@ -0,0 +1,7 @@
+package administrator;
+
+public class DeliveryRunner extends User {
+    public DeliveryRunner(String name, String userId, String email, String username, String password) {
+        super(name, userId, email, username, password, "Runner");
+    }
+}
